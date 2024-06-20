8fc092f18277afb6292120858c9c6a7848a17b2c
SuRrov <pasha-turundaev@yandex.ru>
2024-06-20 14:45:27 +0500

    OOP Main Learn
    ООП (1-5 задание)

--- Основы языка Python/9)Tkinter.py
+++ Основы языка Python/9)Tkinter.py
@@ -4,7 +4,7 @@ root = Tk()     # создаем корневой объект - окно
 root.title("Приложение на Tkinter")     # устанавливаем заголовок окна
 root.geometry("300x250")    # устанавливаем размеры окна
  
-label = Label(text="Hello METANIT.COM") # создаем текстовую метку
-label.pack()    # размещаем метку в окне
+#label = Label(text="Hello METANIT.COM") # создаем текстовую метку
+#label.pack()    # размещаем метку в окне
  
 root.mainloop()
